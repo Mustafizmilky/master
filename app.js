d50e1b88f7442e934fab9c84cf43dbdbb13993f3
Mustafiz Milky <milkyfx@gmail.com>
2023-03-07 14:51:15 +0600

    few change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //calculator project
 const calulationArr = [];
-const arr = [];
+const calArr = [];
 
 const clearText = document.querySelector("#del");
 const inputField = document.querySelector("#input");
@@ -33,8 +33,8 @@ const dot = document.querySelector(".dot").closest(".dot__parent");
 // function to update dom with digit
 const updatetheInput = function (e, value) {
   e.target.textContent = value;
-  arr.push(e.target.textContent);
-  const join = arr.join("");
+  calArr.push(e.target.textContent);
+  const join = calArr.join("");
   inputField.textContent = join;
 };
 // function : variable that fill up from DOM data
@@ -53,7 +53,7 @@ const incrementTheNumber = function (digit, number) {
 //remove the text from the input field
 clearText.addEventListener("click", function (e) {
   inputField.textContent = "0";
-  arr.length = 0;
+  calArr.length = 0;
   location.reload();
 });
 
@@ -77,9 +77,38 @@ equal.addEventListener("click", function (e) {
   inputField.textContent = eval(valueFromDOM);
 
   valueFromDOM = "";
-  arr.length = 0;
+  calArr.length = 0;
 });
 
 // calculator project finished
-console.dir(document);
-console.log("helo");
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (staterIndex, mainIndex) {
+    return [this.starterMenu[staterIndex], this.mainMenu[mainIndex]];
+  },
+};
